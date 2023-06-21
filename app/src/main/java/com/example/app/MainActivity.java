f9f924f58c0258bc25e173e8890839c7bb97dafa
vecna666 <130588389+vecna666@users.noreply.github.com>
2023-06-21 16:18:34 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -20,8 +20,18 @@ public class MainActivity extends Activity {
         webSettings.setJavaScriptEnabled(true);
         mWebView.setWebViewClient(new MyWebViewClient());
 
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.databaseEnabled = true
+        settings.domStorageEnabled = true
+        settings.setSupportMultipleWindows(false)
+        // Zooms out the content to fit on screen by width. For example, showing images.
+        settings.layoutAlgorithm = WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+
         // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+         mWebView.loadUrl("https://github.com");
 
         // LOCAL RESOURCE
         // mWebView.loadUrl("file:///android_asset/index.html");
