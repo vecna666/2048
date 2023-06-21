3d3b0346ccf4d74dd8660267b5283ecc89f7557e
vecna666 <130588389+vecna666@users.noreply.github.com>
2023-06-21 22:51:45 +0200

    Add files via upload

---
+++ app/src/main/assets/nightmode.js
@@ -0,0 +1,26 @@
+function toggleNightMode() {
+  if (document.getElementsByTagName("html")[0].style.backgroundColor === "rgb(45, 48, 44)") {
+    document.getElementsByTagName("html")[0].style.backgroundColor = "#faf8ef";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#faf8ef";
+    if (typeof(Storage) !== "undefined") {
+      localStorage['nightmode'] = "0";
+    }
+    return false;
+  } else {
+    document.getElementsByTagName("html")[0].style.backgroundColor = "#2D302C";
+    document.getElementsByTagName("body")[0].style.backgroundColor = "#2D302C";
+    if (typeof(Storage) !== "undefined") {
+      localStorage['nightmode'] = "1";
+    }
+    return false;
+  }
+}
+
+window.onload = function() {
+  var a = document.getElementById("night");
+  a.onclick = toggleNightMode;
+
+  if (typeof(Storage) !== "undefined" && localStorage['nightmode'] == "1") {
+    toggleNightMode();
+  }
+}
